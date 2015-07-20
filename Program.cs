68e6c7d7da575d00845352d00aaac670d2b29c88
mjsabby <mjsabby@gmail.com>
2015-07-20 02:20:19 -0700

    Add support for Roots and all ObjRefs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,10 @@
 ﻿namespace Dump2SQLite
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.IO;
+    using System.Text;
     using Microsoft.Diagnostics.Runtime;
 
     internal unsafe static class Program
@@ -17,68 +19,121 @@
                 return;
             }
 
+            string dumpLocation = args[0];
+
             Stopwatch watch = new Stopwatch();
             watch.Start();
 
-            DataTarget target = DataTarget.LoadCrashDump(args[0]);
-            ClrInfo version = target.ClrVersions[0];
+            LogInfoWithTimeStamp("Loading Crash Dump from: " + dumpLocation + " ...");
+
+            DataTarget target = DataTarget.LoadCrashDump(dumpLocation);
+            ClrInfo version = target.ClrVersions[0]; // TODO: Probably add support for Multiple CLRs or at least warn we're picking the first
             string dacLocation = version.TryGetDacLocation();
             ClrRuntime runtime = target.CreateRuntime(dacLocation);
             
             var heap = runtime.GetHeap();
-
             if (!heap.CanWalkHeap)
             {
-                Console.WriteLine("ERROR: Heap is not walkable. Please collect a new dump, but force a GC prior to taking the dump.");
+                LogErrorWithTimeStamp("Heap is not walkable. Please collect a new dump.");
+                return;
             }
+
+            LogInfoWithTimeStamp("Crash Dump loaded.");
+
+            var perTypeCounts = new Dictionary<int, int>(100000);
             
             var fileName = Path.GetFullPath(args[0]).Replace(Path.GetExtension(args[0]), ".sqlite");
 
-            Console.WriteLine("SQLite Database Filename: " + fileName);
+            LogInfoWithTimeStamp("Creating SQLite Database filename: " + fileName + " ...");
 
             sqlite3* db;
             int error;
             if ((error = NativeMethods.sqlite3_open(fileName, out db)) != 0)
             {
-                Console.WriteLine("sqlite3_open failed with error code: " + error);
+                LogErrorWithTimeStamp("sqlite3_open failed with error code: " + error);
             }
 
-            CreateTables(db);
+            LogInfoWithTimeStamp("Successfully created SQLite Database filename: " + fileName);
 
-            NativeMethods.sqlite3_exec(db, "BEGIN TRANSACTION;", IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            LogInfoWithTimeStamp("Creating SQLite Tables ...");
+            
+            if (!CreateTables(db))
+            {
+                return;
+            }
 
-            sqlite3_stmt* insertObjectsStmt, insertTypesStmt;
-            char* tail;
+            LogInfoWithTimeStamp("Successfully created SQLite Tables.");
 
-            string insertTypesSql = @"INSERT INTO Types(TypeIndex, MetadataToken, Size, Name) VALUES (@1, @2, @3, @4);";
-            NativeMethods.sqlite3_prepare_v2(db, insertTypesSql, insertTypesSql.Length, out insertTypesStmt, out tail);
+            error = NativeMethods.sqlite3_exec(db, "BEGIN TRANSACTION;", IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            if (error != 0)
+            {
+                LogErrorWithTimeStamp("sqlite3_exec -> BEGIN TRANSACTION; failed to execute with SQLite error code: " + error);
+            }
 
-            string insertObjectsSql = @"INSERT INTO Objects(ObjectId, TypeIndex, Size) VALUES (@1, @2, @3);";
-            NativeMethods.sqlite3_prepare_v2(db, insertObjectsSql, insertObjectsSql.Length, out insertObjectsStmt, out tail);
+            sqlite3_stmt* insertObjectsStmt, insertTypesStmt, insertRootsStmt;
 
+            if (!PrepareInsertStatement(db, out insertTypesStmt, @"INSERT INTO Types(TypeIndex, Count, Size, Name) VALUES (@1, @2, @3, @4);"))
+            {
+                return;
+            }
+
+            if (!PrepareInsertStatement(db, out insertObjectsStmt, @"INSERT INTO Objects(ObjectId, TypeIndex, Size, ObjRefs) VALUES (@1, @2, @3, @4);"))
+            {
+                return;
+            }
+
+            if (!PrepareInsertStatement(db, out insertRootsStmt, @"INSERT INTO Roots(TypeIndex, ObjectId, Address, AppDomainId, ManagedThreadId, IsInterior, IsPinned, IsPossibleFalsePositive, GCRootKind, Name) VALUES (@1, @2, @3, @4, @5, @6, @7, @8, @9, @10);"))
+            {
+                return;
+            }
+
+            LogInfoWithTimeStamp("Starting to populate Objects Table ...");
+
+            var walker = new ReferencesWalker();
             foreach (ulong obj in heap.EnumerateObjects())
             {
                 ClrType type = heap.GetObjectType(obj);
-                
-                if (type == null)
+                if (type != null)
                 {
-                    continue;
+                    type.EnumerateRefsOfObjectCarefully(obj, walker.Walk);
+                    var references = walker.ToString();
+                    walker.Clear();
+
+                    int typeIndex = type.Index;
+                    if (perTypeCounts.ContainsKey(typeIndex))
+                    {
+                        ++perTypeCounts[typeIndex];
+                    }
+                    else
+                    {
+                        perTypeCounts.Add(typeIndex, 1);
+                    }
+
+                    NativeMethods.sqlite3_bind_int64(insertObjectsStmt, 1, (long)obj);
+                    NativeMethods.sqlite3_bind_int(insertObjectsStmt, 2, typeIndex);
+                    NativeMethods.sqlite3_bind_int(insertObjectsStmt, 3, type.BaseSize);
+                    NativeMethods.sqlite3_bind_text(insertObjectsStmt, 4, references, references.Length, NativeMethods.Transient);
+
+                    NativeMethods.sqlite3_step(insertObjectsStmt);
+                    NativeMethods.sqlite3_reset(insertObjectsStmt);
                 }
-                
-                NativeMethods.sqlite3_bind_int64(insertObjectsStmt, 1, (long)obj);
-                NativeMethods.sqlite3_bind_int(insertObjectsStmt, 2, type.Index);
-                NativeMethods.sqlite3_bind_int64(insertObjectsStmt, 3, (long)type.GetSize(obj));
-                
-                NativeMethods.sqlite3_step(insertObjectsStmt);
-                NativeMethods.sqlite3_reset(insertObjectsStmt);
             }
+
+            LogInfoWithTimeStamp("Successfully populated Objects Table.");
+            LogInfoWithTimeStamp("Starting to populate Types Table ...");
 
             foreach (var type in heap.EnumerateTypes())
             {
                 string typeName = type.Name;
+                int typeIndex = type.Index;
+                int count;
+                if (!perTypeCounts.TryGetValue(typeIndex, out count))
+                {
+                    count = 0;
+                }
 
-                NativeMethods.sqlite3_bind_int(insertTypesStmt, 1, type.Index);
-                NativeMethods.sqlite3_bind_int64(insertTypesStmt, 2, type.MetadataToken);
+                NativeMethods.sqlite3_bind_int(insertTypesStmt, 1, typeIndex);
+                NativeMethods.sqlite3_bind_int64(insertTypesStmt, 2, count);
                 NativeMethods.sqlite3_bind_int(insertTypesStmt, 3, type.BaseSize);
                 NativeMethods.sqlite3_bind_text(insertTypesStmt, 4, typeName, typeName.Length, NativeMethods.Transient);
 
@@ -86,32 +141,176 @@
                 NativeMethods.sqlite3_reset(insertTypesStmt);
             }
 
+            LogInfoWithTimeStamp("Successfully populated Types Table.");
+            LogInfoWithTimeStamp("Starting to populate Roots Table ...");
+
+            foreach (var root in heap.EnumerateRoots())
+            {
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 1, root.Type?.Index ?? -1);
+                NativeMethods.sqlite3_bind_int64(insertRootsStmt, 2, (long)root.Object);
+                NativeMethods.sqlite3_bind_int64(insertRootsStmt, 3, (long)root.Address);
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 4, root.AppDomain?.Id ?? -1);
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 5, root.Thread?.ManagedThreadId ?? -1);
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 6, root.IsInterior ? 1 : 0);
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 7, root.IsPinned ? 1 : 0);
+                NativeMethods.sqlite3_bind_int(insertRootsStmt, 8, root.IsPossibleFalsePositive ? 1 : 0);
+
+                string kindString = root.Kind.KindString();
+                string rootName = root.Name;
+
+                NativeMethods.sqlite3_bind_text(insertRootsStmt, 9, kindString, kindString.Length, NativeMethods.Transient);
+                NativeMethods.sqlite3_bind_text(insertRootsStmt, 10, rootName, rootName.Length, NativeMethods.Transient);
+
+                NativeMethods.sqlite3_step(insertRootsStmt);
+                NativeMethods.sqlite3_reset(insertRootsStmt);
+            }
+
+            LogInfoWithTimeStamp("Successfully populated Roots Table.");
+
             NativeMethods.sqlite3_finalize(insertTypesStmt);
             NativeMethods.sqlite3_finalize(insertObjectsStmt);
+            NativeMethods.sqlite3_finalize(insertRootsStmt);
 
-            NativeMethods.sqlite3_exec(db, "END TRANSACTION;", IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            error = NativeMethods.sqlite3_exec(db, "END TRANSACTION;", IntPtr.Zero, IntPtr.Zero, IntPtr.Zero);
+            if (error != 0)
+            {
+                LogErrorWithTimeStamp("sqlite3_exec -> END TRANSACTION; failed to execute with SQLite error code: " + error);
+            }
+
             NativeMethods.sqlite3_close(db);
 
             watch.Stop();
-            Console.WriteLine("Processing time: {0} milliseconds", watch.ElapsedMilliseconds);
+            LogInfoWithTimeStamp("Processing time: " + watch.ElapsedMilliseconds + "  milliseconds");
         }
 
-        private static void CreateTables(sqlite3* db)
+        private static bool PrepareInsertStatement(sqlite3* db, out sqlite3_stmt* stmt, string sql)
+        {
+            char* tail;
+            int error = NativeMethods.sqlite3_prepare_v2(db, sql, sql.Length, out stmt, out tail);
+
+            if (error != 0)
+            {
+                LogErrorWithTimeStamp("sqlite3_prepare_v2 -> " + sql + " failed to execute with SQLite error code: " + error);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static bool CreateTable(sqlite3* db, string sql)
         {
             sqlite3_stmt* createTableStmt;
             char* tail;
+            
+            int error = NativeMethods.sqlite3_prepare_v2(db, sql, sql.Length, out createTableStmt, out tail);
 
-            string createSql = @"CREATE TABLE Objects(ObjectId INTEGER PRIMARY KEY, TypeIndex INTEGER, Size INTEGER);";
+            if (error != 0)
+            {
+                LogErrorWithTimeStamp("sqlite3_prepare_v2 -> " + sql + " failed to execute with SQLite error code: " + error);
+                NativeMethods.sqlite3_finalize(createTableStmt);
+                return false;
+            }
 
-            NativeMethods.sqlite3_prepare_v2(db, createSql, createSql.Length, out createTableStmt, out tail);
-            NativeMethods.sqlite3_step(createTableStmt);
+            error = NativeMethods.sqlite3_step(createTableStmt);
 
-            createSql = @"CREATE TABLE Types(TypeIndex INTEGER PRIMARY KEY, MetadataToken INTEGER, Size INTEGER, Name TEXT);";
-
-            NativeMethods.sqlite3_prepare_v2(db, createSql, createSql.Length, out createTableStmt, out tail);
-            NativeMethods.sqlite3_step(createTableStmt);
+            if (error != 101)
+            {
+                LogErrorWithTimeStamp("sqlite3_step -> " + sql + " failed to execute with SQLite error code: " + error);
+                NativeMethods.sqlite3_finalize(createTableStmt);
+                return false;
+            }
 
             NativeMethods.sqlite3_finalize(createTableStmt);
+            return true;
+        }
+
+        private static string KindString(this GCRootKind kind)
+        {
+            switch (kind)
+            {
+                case GCRootKind.StaticVar:
+                    return "StaticVar";
+                case GCRootKind.Finalizer:
+                    return "Finalizer";
+                case GCRootKind.Pinning:
+                    return "Pinning";
+                case GCRootKind.AsyncPinning:
+                    return "AsyncPinning";
+                case GCRootKind.LocalVar:
+                    return "LocalVar";
+                case GCRootKind.ThreadStaticVar:
+                    return "ThreadStaticVar";
+                case GCRootKind.Strong:
+                    return "Strong";
+                case GCRootKind.Weak:
+                    return "Weak";
+                default:
+                    return "Unknown";
+            }
+        }
+
+        private static bool CreateTables(sqlite3* db)
+        {
+            if (!CreateTable(db, @"CREATE TABLE Objects(ObjectId INTEGER PRIMARY KEY, TypeIndex INTEGER, Size INTEGER, ObjRefs TEXT);"))
+            {
+                return false;
+            }
+
+            if (!CreateTable(db, @"CREATE TABLE Types(TypeIndex INTEGER PRIMARY KEY, Count INTEGER, Size INTEGER, Name TEXT);"))
+            {
+                return false;
+            }
+
+            if (!CreateTable(db, "CREATE TABLE Roots(TypeIndex INTEGER, ObjectId INTEGER, Address INTEGER, AppDomainId INTEGER, ManagedThreadId INTEGER, IsInterior BOOLEAN, IsPinned BOOLEAN, IsPossibleFalsePositive BOOLEAN, GCRootKind TEXT, Name TEXT);"))
+            {
+                return false;
+            }
+
+            return true;
+        }
+        
+        private static void LogErrorWithTimeStamp(string message)
+        {
+            Console.WriteLine("[ERROR][{0}]: {1}", DateTime.Now, message);
+        }
+
+        private static void LogInfoWithTimeStamp(string message)
+        {
+            Console.WriteLine("[INFO][{0}]: {1}", DateTime.Now, message);
+        }
+
+        private sealed class ReferencesWalker
+        {
+            private readonly List<ulong> References = new List<ulong>(100);
+
+            private readonly StringBuilder sb = new StringBuilder(1024 * 10);
+
+            public void Walk(ulong @object, int fieldOffset)
+            {
+                this.References.Add(@object);
+            }
+
+            public void Clear()
+            {
+                this.References.Clear();
+                this.sb.Clear();
+            }
+
+            public override string ToString()
+            {
+                int count = this.References.Count;
+                for (int i = 0; i < count; ++i)
+                {
+                    this.sb.Append(this.References[i]);
+
+                    if (count - i != 1)
+                    {
+                        this.sb.Append(',');
+                    }
+                }
+
+                return this.sb.ToString();
+            }
         }
     }
 }
